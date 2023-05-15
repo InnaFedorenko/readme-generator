366168fc7a080bdf66afe1b25869a44e8922df07
Inna Fedorenko <ivf.fedorenko@gmail.com>
2023-05-15 17:45:47 -0400

    added modules integration

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+// console.log(inquirer);
+const renerateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -7,7 +10,10 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    console.log("Welcome to the password generator! :-)");
+    console.log(renerateMarkdown.renderLicenseBadge('MIT'))
+}
 
 // Function call to initialize app
 init();
